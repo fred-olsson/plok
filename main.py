d4d00d1211edbec14f424907d94fc389391d045c
fred <olsson.fred@gmail.com>
2019-04-21 19:47:37 +0200

    testing Fernet

--- main.py
+++ main.py
@@ -20,6 +20,12 @@ def main(loc_id):
         print(tmp_pw[loc_id])
     else:
         exit('Unauthorized!')
+    test1 = encrypt_pass('asdf', tmp_pw['skeleton'])
+    print(test1)
+    test1 = test1.decode()
+    test2 = decrypt_pass(test1, 'Dm1We0qIUPgaesn/Gs2nAmGKKcHMTegvMm/tO8ou0p3=')
+    print(test2)
+    #unmatching keys still decrypt? Only if changed at the end
  
 #get user input and return it as a byte-object
 def get_pass():
@@ -42,6 +48,19 @@ def gen_hash(user_pass):
         backend=default_backend()
     )
     return base64.b64encode(kdf.derive(user_pass))
+
+#encrypt/decrypt passwords using Fernet. Returns a b64 byte-object
+def encrypt_pass(unenc_pass, key):
+    key = key.encode()
+    unenc_pass = unenc_pass.encode()
+    fer = Fernet(key)
+    return fer.encrypt(unenc_pass)
+    
+def decrypt_pass(enc_pass, key):
+    key = key.encode()
+    enc_pass = enc_pass.encode()
+    fer = Fernet(key)
+    return fer.decrypt(enc_pass)
 
 if __name__ == '__main__':
     main(sys.argv[1])
